f1389d9f6187ed1e07012f0eb45966d096bd1941
PshenichnayaE <90998069+PshenichnayaE@users.noreply.github.com>
2022-11-20 12:38:43 +1000

    асинхронность/сеть/оптимизация

--- js/map.js
+++ js/map.js
@@ -1,60 +1,91 @@
-import {renderCard} from './card.js';
-import {setActiveAdForm, getAddress} from './form.js';
-import {ZOOM} from './const.js';
+import { renderCard } from './card.js';
+import { ZOOM } from './const.js';
 
-const pinIcon = L.icon({
-  iconUrl: './img/pin.svg',
-  iconSize: [40, 40],
-  iconAnchor: [20, 40],
-});
+const DEFAULT_PIN_PATH = './img/pin.svg';
+const DEFAULT_PIN_ICON_SIZE = [40, 40];
+const DEFAULT_PIN_ANCHOR_SIZE = [20, 40];
 
-const mainPinIcon = L.icon({
-  iconUrl: './img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
-});
+const MAIN_PIN_PATH = './img/main-pin.svg';
+const MAIN_PIN_ICON_SIZE = [52, 52];
+const MAIN_PIN_ANCHOR_SIZE = [26, 52];
 
-// Добавление марки и балуна
-export function createMarker(offers, markerGroup) {
+const TILE_LAYER_LINK = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const TILE_LAYER_ATTRIBUTION = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const IconType = {
+  MAIN: 'main',
+  DEFAULT: 'default'
+};
+
+function createPinIcon(iconType) {
+  switch (iconType) {
+    case (IconType.DEFAULT):
+      return L.icon({
+        iconUrl: DEFAULT_PIN_PATH,
+        iconSize: DEFAULT_PIN_ICON_SIZE,
+        iconAnchor: DEFAULT_PIN_ANCHOR_SIZE,
+      });
+    case (IconType.MAIN):
+      return L.icon({
+        iconUrl: MAIN_PIN_PATH,
+        iconSize: MAIN_PIN_ICON_SIZE,
+        iconAnchor: MAIN_PIN_ANCHOR_SIZE,
+      });
+    default:
+      throw new Error('Unknown pin icon type');
+  }
+}
+
+function createMarker(location, iconType, isDraggable = false) {
+  const {lat, lng} = location;
+  return L.marker(
+    {lat, lng},
+    {
+      icon: createPinIcon(iconType),
+      draggable: isDraggable
+    },
+  );
+}
+
+function removeMarkers(map) {
+  map.eachLayer((layer) => {
+    const {options} = layer;
+    const {icon} = options;
+    if (icon && icon.options.iconUrl === DEFAULT_PIN_PATH) {
+      layer.remove();
+    }
+  });
+}
+
+export function renderMarkers(map, offers) {
+  removeMarkers(map);
   offers.forEach((offer) => {
     const cardElement = renderCard(offer);
-    const marker = L.marker(
-      { lat: offer.location.lat, lng: offer.location.lng },
-      { icon: pinIcon }
-    );
-
-    marker
-      .addTo(markerGroup)
+    createMarker(offer.location, IconType.DEFAULT)
+      .addTo(map)
       .bindPopup(cardElement);
   });
 }
 
-// Отображение карты
-export function initMap(mapElement, addressElement, coordinates) {
+export function initMap(mapElement, coordinates, onLoadMap, onPinMoveEnd) {
   const map = L.map(mapElement)
-    .on('load', () => {
-      getAddress(addressElement, coordinates);
-      setActiveAdForm(true);
-    })
+    .on('load', onLoadMap)
     .setView(coordinates, ZOOM);
 
-  L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    },
-  ).addTo(map);
+  L.tileLayer(TILE_LAYER_LINK, { attribution: TILE_LAYER_ATTRIBUTION })
+    .addTo(map);
 
-  const mainPinMarker = L.marker(
-    coordinates, { draggable: true, icon: mainPinIcon, },
-  );
+  const mainMarkerGroup = L
+    .layerGroup()
+    .addTo(map);
 
-  const markerGroup = L.layerGroup().addTo(map);
-  mainPinMarker.addTo(markerGroup);
+  const mainPinMarker = createMarker(coordinates, IconType.MAIN, true);
+  mainPinMarker.addTo(mainMarkerGroup);
+  mainMarkerGroup.on('moveend', onPinMoveEnd);
 
-  mainPinMarker.on('moveend', ({target}) => {
-    getAddress(addressElement, target.getLatLng());
-  });
-
-  return map;
+  return {
+    map,
+    mainPinMarker,
+  };
 }
+
--- js/util.js
+++ js/util.js
@@ -1,30 +1,4 @@
-import {ALERT_SHOW_TIME} from './const.js';
-
-// Получение случайного целого числа из переданного диапазона включительно
-export function getRandomPositiveInteger (a, b) {
-  if (a < 0 || b < 0) {
-    return NaN;
-  }
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
-
-// Получение случайного числа с плавающей точкой из переданного диапазона включительно
-export function getRandomPositiveFloat (a, b, digits = 1) {
-  if (a < 0 || b < 0 || digits < 0) {
-    return NaN;
-  }
-  const lower = Math.min(a, b);
-  const upper = Math.max(a, b);
-  const result = Math.random() * (upper - lower) + lower;
-  return +result.toFixed(digits);
-}
-
-export function getRandomArrayElement (elements) {
-  return elements[getRandomPositiveInteger(0, elements.length - 1)];
-}
+import { ALERT_SHOW_TIME } from './const.js';
 
 export function isEscapeKey (evt) {
   evt.key = 'Escape';
@@ -48,3 +22,13 @@ export const showAlert = (message) => {
     alert.remove();
   }, ALERT_SHOW_TIME);
 };
+
+// Функция debounce для устранения дребезга
+export function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
